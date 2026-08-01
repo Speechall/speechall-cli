c0b11c1117bac19b1bdbd078f8f99e7a73527684
atacan <atacandur@icloud.com>
2026-08-01 15:34:00 +0200

    bump version to 0.2.0

--- Sources/SpeechallCLI/SpeechallCLI.swift
+++ Sources/SpeechallCLI/SpeechallCLI.swift
@@ -96,7 +96,7 @@ public struct Speechall: AsyncParsableCommand {
 
             Set SPEECHALL_API_KEY environment variable or pass --api-key.
             """,
-        version: "0.1.1",
+        version: "0.2.0",
         subcommands: [Transcribe.self, Models.self],
         defaultSubcommand: Transcribe.self
     )
